c0d216a4a9d42871347249f2768374207830ac31
Ruslan Sakulin <ruslansakulin@gmail.com>
2022-11-30 20:09:21 +0300

    Example 010

--- example010/Program.cs
+++ example010/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+﻿int[] array = {1, 12, 31, 4, 15, 16, 4, 18};
 
 int n = array.Length;
 int find = 4;
@@ -7,7 +7,6 @@ int index = 0;
 
 while (index < n)
 {
-
     if(array[index] == find)
     {
         Console.WriteLine(index);
