4b1a52786230f91cba7d44a2b89e91bd367a4a64
Ruslan Sakulin <ruslansakulin@gmail.com>
2022-11-30 19:53:01 +0300

    Example 080 upgrade

--- example008/Program.cs
+++ example008/Program.cs
@@ -7,18 +7,20 @@
 }
 int a1 = 15;
 int b1 = 21;
-int c1 = 39;
+int c1 = 39111;
 int a2 = 12;
 int b2 = 2311;
 int c2 = 33;
 int a3 = 13;
-int b3 = 23;
+int b3 = 23111;
 int c3 = 11;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine(max);
 
